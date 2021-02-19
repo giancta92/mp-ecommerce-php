c55834a929c295abf0905f4463f8107118fc1942
Gian <giancta92>
2021-02-19 14:04:05 -0500

    agregado boton default MP

--- detail.php
+++ detail.php
@@ -130,7 +130,29 @@
                                             <?php echo "$" . $_POST['unit'] ?>
                                         </h3>
                                     </div>
+                                    <?php
+                                    // SDK de Mercado Pago
+                                    require __DIR__ .  '/sdk-mp/vendor/autoload.php';
+
+                                    // Agrega credenciales
+                                    MercadoPago\SDK::setAccessToken('APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439');
+
+                                    // Crea un objeto de preferencia
+                                    $preference = new MercadoPago\Preference();
+
+                                    // Crea un ítem en la preferencia
+                                    $item = new MercadoPago\Item();
+                                    $item->title = 'Mi producto';
+                                    $item->quantity = 1;
+                                    $item->unit_price = 75.56;
+                                    $preference->items = array($item);
+                                    $preference->save();
+                                    ?>
                                     <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>
+                                    <script
+                                        src="https://www.mercadopago.com.pe/integrations/v1/web-payment-checkout.js"
+                                        data-preference-id="<?php echo $preference->id; ?>">
+                                    </script>
                                 </div>
                             </div>
                         </div>
